3f356919fbd38b7cfa81c52317b8972cf106ae5c
Carlos Junior <carlos.junior@icolabora.com.br>
2020-01-27 11:00:24 -0300

    Basic Structure

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,6 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'D&D Manager'
 }
 </script>
--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -3,19 +3,31 @@
 
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="left = !left" />
+        <q-btn dense flat round icon="fas fa-bars" @click="left = !left" />
 
         <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
-          </q-avatar>
-          Title
+          {{ name }}
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
     <q-drawer show-if-above v-model="left" side="left" bordered>
-      <!-- drawer content -->
+      <q-scroll-area class="fit">
+          <q-list v-for="(menuItem, index) in menuList" :key="index">
+
+            <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+              <q-item-section avatar>
+                <q-icon :name="menuItem.icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ menuItem.label }}
+              </q-item-section>
+            </q-item>
+
+           <q-separator v-if="menuItem.separator" />
+
+          </q-list>
+        </q-scroll-area>
     </q-drawer>
 
     <q-page-container>
@@ -29,7 +41,35 @@
 export default {
   data () {
     return {
-      left: false
+      left: false,
+      name: 'D&D Manager',
+      menuList: [
+        {
+          icon: 'fas fa-home',
+          label: 'Home',
+          separator: true
+        },
+        {
+          icon: 'fas fa-user-alt',
+          label: 'Players',
+          separator: false
+        },
+        {
+          icon: 'fas fa-user-friends',
+          label: 'NPCs',
+          separator: false
+        },
+        {
+          icon: 'fas fa-dragon',
+          label: 'Monsters',
+          separator: true
+        },
+        {
+          icon: 'fas fa-dice-d20',
+          label: 'Initiative Tracker',
+          separator: true
+        }
+      ]
     }
   }
 }
